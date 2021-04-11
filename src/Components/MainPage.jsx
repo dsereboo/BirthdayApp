2dcfce2a30456b8b2be5d519e287b80b84c049a4
Daniel A-Sereboo <dsereboo@gmail.com>
2021-04-11 19:29:18 +0000

    UseEffect hook to load birthdays of current month

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -1,10 +1,20 @@
 import axios from "axios"
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import {Link} from "react-router-dom"
 import {Form, Button, Row, Col, Container} from "react-bootstrap"
 
 const MainPage =()=>{
     const[query, setQuery]= useState("")
+
+    useEffect(
+        ()=>{
+            var DayDate= new Date()
+            //Returns current month in full text 
+            var Month=new Intl.DateTimeFormat('en-US', {month:"long"}).format(DayDate)
+            axios.get(`https://sheet.best/api/sheets/566ada79-9156-4425-a5f2-f9054003de35/monthOfBirth/*${Month}*`)
+            .then(response=>{console.log(response)})
+        }
+    )     
 
     const handleChange=(event)=>{
         setQuery(event.target.value)
@@ -22,20 +32,19 @@ const MainPage =()=>{
         )
     }
 
-
     return(
            <Container fluid>
                <Row>
                    <Col md="2">
                        <p>Sidebar</p>
-                       <Link to="/addUser"><Button variant="danger" block><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus" viewBox="0 0 16 16">
+                       <Link to="/addUser"><Button variant="danger" block><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-plus" viewBox="0 0 16 16">
                         <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
                     </svg>Add a birthday</Button></Link>
                        <h5>View Birthdays</h5>
                        <ul>
-                            <li>This Month</li>
-                            <li>Next Month</li>
-                            <li>All Months</li>
+                            <li><Button>This Month</Button></li>
+                            <li><Button>Next Month</Button></li>
+                            <li><Button>All Months</Button></li>
                        </ul>
                        
                    </Col>
